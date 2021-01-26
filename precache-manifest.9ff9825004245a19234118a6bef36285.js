96e2c2fd6b22b8b6195c08d8c73ec93913d124e4
boothscript <boothscript@users.noreply.github.com>
2021-01-26 15:30:31 +0000

    deploy: f70a99ffa9cc9765bdc318bb6246d9d4436329ae

---
+++ precache-manifest.9ff9825004245a19234118a6bef36285.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d2433d4ab01c5e7898855c72520509",
+    "url": "/west-trump-swift/index.html"
+  },
+  {
+    "revision": "76993630b291c74e3f3f",
+    "url": "/west-trump-swift/static/js/2.fb4413ac.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/west-trump-swift/static/js/2.fb4413ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba6978f1ff8ae35f9ad",
+    "url": "/west-trump-swift/static/js/main.fe57d75f.chunk.js"
+  },
+  {
+    "revision": "59ed71f1f43ec3afdb86",
+    "url": "/west-trump-swift/static/js/runtime-main.2ab3e4cc.js"
+  },
+  {
+    "revision": "cac70a8818763c714f00e4181c4897a4",
+    "url": "/west-trump-swift/static/media/swift.cac70a88.svg"
+  },
+  {
+    "revision": "fd32fc385e9be0eb63b15f5eba5bfd09",
+    "url": "/west-trump-swift/static/media/trump.fd32fc38.svg"
+  },
+  {
+    "revision": "a428f00876035f4cc837aa1c2477eb29",
+    "url": "/west-trump-swift/static/media/west.a428f008.svg"
+  }
+]);
